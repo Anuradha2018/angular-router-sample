b048750c2c53edd61149b35d23f6af747f185930
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-12-12 17:24:46 +0100

    hero-list added

--- angular-router-sample/src/app/heroes/hero-list/hero-list.component.ts
+++ angular-router-sample/src/app/heroes/hero-list/hero-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
+
+import { HeroService }  from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-list',
@@ -6,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-list.component.css']
 })
 export class HeroListComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  selectedId: number;
 
-  constructor() { }
+  constructor(
+    private service: HeroService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.heroes$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        // (+) before `params.get()` turns the string into a number
+        this.selectedId = +params.get('id');
+        return this.service.getHeroes();
+      })
+    );
   }
 
 }
