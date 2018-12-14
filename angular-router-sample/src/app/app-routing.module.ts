0bba341be7002df46d1021dff3302f454bdc95b9
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-12-14 15:09:35 +0100

    admin folder with sub components ready

--- angular-router-sample/src/app/app-routing.module.ts
+++ angular-router-sample/src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
-import { HeroesModule } from './heroes/heroes.module';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes }  from '@angular/router';
- 
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ComposeMessageComponent }  from './compose-message/compose-message.component';
+import { PageNotFoundComponent }    from './page-not-found/page-not-found.component';
+
+import { AuthGuard }                          from './auth/auth.guard';
+import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
 
 const appRoutes: Routes = [
-  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  {
+    path: 'compose',
+    component: ComposeMessageComponent,
+    outlet: 'popup'
+  },
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#AdminModule',
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'crisis-center',
+    loadChildren: './crisis-center/crisis-center.module#CrisisCenterModule',
+    data: { preload: true }
+  },
+  { path: '',   redirectTo: '/superheroes', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
@@ -14,12 +31,14 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      {
+        enableTracing: false, // <-- debugging purposes only
+        preloadingStrategy: SelectivePreloadingStrategyService,
+      }
     )
   ],
   exports: [
     RouterModule
-  ],
-  declarations: []
+  ]
 })
 export class AppRoutingModule { }
