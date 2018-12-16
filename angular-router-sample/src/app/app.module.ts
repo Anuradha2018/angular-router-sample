dff9ad3a0a4fea48f743764050139b411ef343ae
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-12-16 19:30:19 +0100

    all done

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -1,43 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule }   from '@angular/common';
-
-
-import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-center/crisis-list/crisis-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HeroesModule } from './heroes/heroes.module';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CrisisCenterComponent } from './crisis-center/crisis-center/crisis-center.component';
-import { CrisisCenterHomeComponent } from './crisis-center/crisis-center-home/crisis-center-home.component';
-import { CrisisDetailComponent } from './crisis-center/crisis-detail/crisis-detail.component';
-import { CrisisCenterModule }      from './crisis-center/crisis-center.module';
+
+import { Router } from '@angular/router';
+
+import { AppComponent }            from './app.component';
+import { PageNotFoundComponent }   from './page-not-found/page-not-found.component';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
-import { LoginComponent } from './auth/login/login.component';
+
+import { AppRoutingModule }        from './app-routing.module';
+import { HeroesModule }            from './heroes/heroes.module';
+import { AuthModule }              from './auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CrisisListComponent,
-    PageNotFoundComponent,
-    CrisisCenterComponent,
-    CrisisCenterHomeComponent,
-    CrisisDetailComponent,
-    ComposeMessageComponent,
-    LoginComponent
-  ],
   imports: [
-    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HeroesModule,
-    CrisisCenterModule,
-    AppRoutingModule
+    AuthModule,
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    ComposeMessageComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    // Use a custom replacer to display function names in the route configs
+    // const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+
+    // console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
+}
+
