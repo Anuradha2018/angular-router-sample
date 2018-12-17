f1d0fec548a2fc14a8855b88c84961e27913e243
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-12-17 13:20:04 +0100

    exlainations

--- angular-router-sample/src/app/heroes/hero-list/hero-list.component.ts
+++ angular-router-sample/src/app/heroes/hero-list/hero-list.component.ts
@@ -24,7 +24,8 @@ export class HeroListComponent implements OnInit {
     this.heroes$ = this.route.paramMap.pipe(
       switchMap(params => {
         // (+) before `params.get()` turns the string into a number
-        this.selectedId = +params.get('id');
+        this.selectedId = +params.get('id'); // You need a way to detect when the route parameters change from within the same instance. 
+                                             //The observable paramMap property handles that beautifully.
         return this.service.getHeroes();
       })
     );
